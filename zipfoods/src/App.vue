c084944b45695dcc8a5fa3145208ec1173a0e064
terichadbourne <terichad@gmail.com>
2019-11-06 16:02:41 -0500

    BROKEN after wk 9 part 3 33:41

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div id="app">
-    <img id="logo" alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png">
+    <img id="logo" alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png" />
     <p>ZipFoods is your one-stop shop for convenient online grocery shopping in the greater Boston area.</p>
+    <ShowProducts></ShowProducts>
   </div>
 </template>
 
 <script>
-
+import ShowProducts from "./components/ShowProducts.vue";
 
 export default {
-  name: 'app',
+  name: 'ShowProducts',
   components: {
-
+    ShowProducts
   }
-}
+};
 </script>
 
 <style lang='scss'>
-@import './assets/css/zipfoods.scss';
+@import "./assets/css/zipfoods.scss";
 </style>
---
+++ zipfoods/src/components/ShowProduct.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="product">
+    <div class="product-name">{{ product.name}}</div>
+    <img
+      class="product-thumb"
+      :alt='"Product image of {{product.name}}"'
+      :src='"./../assets/images/products/" + product.id + ".jpg"'
+    />
+    <p class="product-description">{{ product.description}}</p>
+    <div class="product-price">${{ product.price}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowProduct",
+  props: ["product"]
+};
+</script>
+
+<style scoped>
+</style>
