ce817b17112afc77c6b4a38c601923af999c8b12
terichadbourne <terichad@gmail.com>
2019-12-19 13:49:00 -0500

    move custom Cypress commands to commands.js

--- p4/tests/e2e/specs/p4.js
+++ p4/tests/e2e/specs/p4.js
@@ -2,37 +2,7 @@
 // produce both rants and raves, so it's not necessary to test all features on both 
 // types of feedback.
 
-// ***********************************************************************
-// To preserve favorite status throughout, I borrowed this hack to 
-// preserve localStorage between Cypress tests from @pietmichal 
-// https://github.com/cypress-io/cypress/issues/461#issuecomment-392070888
-// as surfaced in 
-// https://stackoverflow.com/questions/50471047/preserve-cookies-localstorage-session-across-tests-in-cypress
-
-
-let LOCAL_STORAGE_MEMORY = {};
-
-Cypress.Commands.add("saveLocalStorage", () => {
-  Object.keys(localStorage).forEach(key => {
-    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
-  });
-});
-
-Cypress.Commands.add("restoreLocalStorage", () => {
-  Object.keys(LOCAL_STORAGE_MEMORY).forEach(key => {
-    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
-  });
-});
-
-beforeEach(() => {
-  cy.restoreLocalStorage();
-});
-
-afterEach(() => {
-  cy.saveLocalStorage();
-});
-
-// **********************************************************************
+// NOTE: See support/commands.js for how localStorage is being maintained across tests. 
 
 describe('test all the things!', () => {
 
--- p4/tests/e2e/support/commands.js
+++ p4/tests/e2e/support/commands.js
@@ -1,25 +1,31 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// To preserve favorite status throughout, I borrowed this hack to 
+// preserve localStorage between Cypress tests from @pietmichal 
+// https://github.com/cypress-io/cypress/issues/461#issuecomment-392070888
+// as surfaced in 
+// https://stackoverflow.com/questions/50471047/preserve-cookies-localstorage-session-across-tests-in-cypress
+
+let LOCAL_STORAGE_MEMORY = {};
+
+Cypress.Commands.add("saveLocalStorage", () => {
+  Object.keys(localStorage).forEach(key => {
+    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+  });
+});
+
+Cypress.Commands.add("restoreLocalStorage", () => {
+  Object.keys(LOCAL_STORAGE_MEMORY).forEach(key => {
+    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+  });
+});
+
+beforeEach(() => {
+    cy.restoreLocalStorage();
+  });
+  
+  afterEach(() => {
+    cy.saveLocalStorage();
+  });
